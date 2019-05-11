71f7fe63ff6f12cc12751639f3a664086d8221c3
Sunny Wong <swong6@mail.sfsu.edu>
2019-05-10 23:38:12 -0700

    Added multer module for uploading an image; image uploads works

--- csc648verticalprototype/post.js
+++ csc648verticalprototype/post.js
@@ -2,6 +2,51 @@
 var express = require('express');
 var router = express.Router();
 var db = require('./db');
+const multer = require('multer');
+const ejs = require('ejs');
+const path = require('path');
+
+// Public folder
+router.use(express.static('./public'));
+
+// Set storage engine
+const storage = multer.diskStorage({
+    destination:'./public/img/',
+    filename: function(req,file,cb){
+        cb(null,file.fieldname + '-' + Date.now() + 
+        // Sets the extension from the uploaded file to this new filepath i.e. jpg will stay jpg
+        path.extname(file.originalname));
+    }
+});
+// Init upload 
+const upload = multer({
+    //Directs the file upload to the /public/uploads folder
+    storage: storage,
+    // Limit file size to 10MB
+    limits: {fileSize: 10*1024*1024},
+    fileFilter: function(req, file, cb){
+        checkFileType(file, cb);
+    }
+    // Single image upload and retrieves the name tag attribute from EJS
+}).single('image');
+
+//Check file type
+function checkFileType(file, cb){
+    // Allowed extensions
+    const fileTypes = /jpeg|jpg|png/;
+    // Check file extension
+    const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
+    // Check mime
+    const mimeType = file.mimetype.startsWith('image/');
+
+    if(mimeType && extname){
+        return cb(null, true);
+    } else {
+        cb('Error: Images Only!');
+    }
+}
+
+
 
 router.get('/', (req, res) => {
     // USING DUMMY POST EJS, CHANGE LATER
@@ -37,42 +82,76 @@ router.post('/', userPost, (req, res) => {
 
 // For registered user to be able to post; need to move to user folder
 function userPost(req, res) {
-    var id = req.user;
-    var postName = req.body.title;
-    var price = req.body.price;
-    var beds = req.body.beds;
-    var baths = req.body.baths;
-    var category = req.body.category;
-    var location = req.body.location;
-    var city = req.body.city;
-    var state = req.body.state;
-    var phone = req.body.phone;
-    var email = req.body.email;
-    var description = req.body.description;
-    var image = req.body.image;
+    
 
     if (req.user != undefined) {
-        console.log("post user id is: " + id);
-        let query = ` INSERT INTO post (post_name, price, beds, baths, category, user_id, location, city, state, phone, email, description, image)
-        VALUES('${postName}', '${price}', '${beds}', '${baths}', '${category}', '${id}', '${location}', '${city}', '${state}', '${phone}', 
-            '${email}', '${description}', '${image}') `;
-
-        console.log(query);
-        db.query(query, (err, result) => {
+        upload(req, res, (err) => {
             if (err) {
-                console.log("Failed to insert into post table: " + err)
-            }
-            console.log("Inserted row: " + result);
+                res.render('dummyPost', {
+                    // Ejs variables being passed into index.ejs
+                    errors: err,
+                    results: 0,
+                    searchTerm: "",
+                    searchResult: "",
+                    searchCategory: "",
+                    sortType: "",
+                    priceFilter: "",
+                    distanceFilter: ""
+                });
+            } else {
+                if (req.file == undefined) {
+                    res.render('dummyPost', {
+                        // Ejs variables being passed into index.ejs
+                        errors: 'Error: No File Selected!',
+                        results: 0,
+                        searchTerm: "",
+                        searchResult: "",
+                        searchCategory: "",
+                        sortType: "",
+                        priceFilter: "",
+                        distanceFilter: ""
+                    });
+                } else {
+                    var id = req.user[0].id;
+                    var postName = req.body.title;
+                    var price = req.body.price;
+                    var beds = req.body.beds;
+                    var baths = req.body.baths;
+                    var category = req.body.category;
+                    var location = req.body.location;
+                    var city = req.body.city;
+                    var state = req.body.state;
+                    var phone = req.body.phone;
+                    var email = req.body.email;
+                    var description = req.body.description;
+                    var image = req.file.filename;
 
+                    // Handle upload to database.
+                    console.log("post user id is: " + id);
+                    let query = ` INSERT INTO post (post_name, price, beds, baths, category, user_id, location, city, state, phone, email, description, image)
+                                   VALUES('${postName}', '${price}', '${beds}', '${baths}', '${category}', '${id}', '${location}', '${city}', '${state}', '${phone}', 
+                                          '${email}', '${description}', '${image}') `;
+
+                    console.log(query);
+                    db.query(query, (err, result) => {
+                        if (err) {
+                            console.log("Failed to insert into post table: " + err)
+                        }
+                        console.log("Inserted row: " + result);
+
+                    });
+                    req.flash('success', "Successfully submitted a post! Please wait at least 24 hours to give us a chance to review your post!")
+                    res.redirect('/');
+                }
+            }
         });
-        req.flash('success', "Successfully submitted a post! Please wait at least 24 hours to give us a chance to review your post!")
-        res.redirect('/');
+
     } else {
         req.flash('danger', "Please login to post");
         res.redirect('/login');
     }
 
-}
+    }
 
 
 module.exports = router;
